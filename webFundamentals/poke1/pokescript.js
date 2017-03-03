1a213d38c3b8f0257ba7cac04b348cbad3c4398d
Rob Matthews <rob.matthews@me.com>
2017-03-02 23:23:18 -0600

    Ajax pokemon

--- webFundamentals/poke1/pokescript.js
+++ webFundamentals/poke1/pokescript.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     var poke;
     for (var i = 1; i <= 151; i++){
-        poke = "<img src='http://pokeapi.co/media/img/" + i + ".png'>";
-        $(".container").append(poke);
-    } 
+        poke = "<img id='" + i + "' src='http://pokeapi.co/media/img/" + i + ".png'>";
+        $(".left-side").append(poke);
+    }
+     
 })
+
+
+
