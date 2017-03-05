6721b861459be26ee9f6d17a87ff12ad6edc1a59
Rob Matthews <rob.matthews@me.com>
2017-03-05 17:00:10 -0600

    32% done with Python

---
+++ Python/pythonFile.py
@@ -0,0 +1,21 @@
+hockeyStickBrand = "Easton"
+
+print hockeyStickBrand
+
+hockeyStickBrand = "Bauer"
+
+print hockeyStickBrand
+
+print 10*8+8
+
+string1 = "This string will connect to"
+string2 = " this string"
+
+print string1 + string2
+
+
+string3 = 4
+
+string3
+
+print string1 + str(string3)
