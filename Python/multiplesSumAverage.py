2cabab8fd85f4c2eaba86201f90955b656aec55e
Rob Matthews <rob.matthews@me.com>
2017-03-05 22:53:02 -0600

    Finish assignment covering basic looping in Python

---
+++ Python/multiplesSumAverage.py
@@ -0,0 +1,23 @@
+for count in range(1, 1000, 2):
+    print count
+
+
+for count in range(5, 1000000, 5):
+    print count
+
+
+
+sum = 0
+a = [1, 2, 5, 10, 255, 3];
+for element in a:
+    sum += element
+print sum
+
+
+
+sum = 0
+a = [1, 2, 5, 10, 255, 3];
+for element in a:
+    sum += element
+avg = sum/len(a)
+print avg
