ea25cf2e64c00ea212e3cf4443c8baab95807c5f
Rob Matthews <rob.matthews@me.com>
2017-03-06 10:39:08 -0600

    Add files to delete manually

--- poke1/pokescript.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var poke;
-    for (var i = 1; i <= 151; i++){
-        poke = ("<img src='http://pokeapi.co/media/img/" + i + ".png'>");
-    } $(".container").append(poke);
-})
