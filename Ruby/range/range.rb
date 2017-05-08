47bb96fb28006d5cb7028ce82e00cf7c4b3c3fd8
Rob Matthews <rob.matthews@me.com>
2017-05-08 11:38:03 -0500

    finished range assignment

---
+++ Ruby/range/range.rb
@@ -0,0 +1,21 @@
+# .include?(value) => true or false
+
+# .last => returns the last object in range
+
+# .max => returns the maximum value in range
+
+# .min => returns the minimum value in range
+
+
+the_range = (1..100)
+
+
+puts the_range
+
+puts the_range.include?(3)
+
+puts the_range.last
+
+puts the_range.max
+
+puts the_range.min
